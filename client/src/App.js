99bd2dc1e54043a5f7592ccd3d4c171e74797b63
poojith kumar <poojithk956@gmail.com>
2024-03-11 01:11:12 +0530

    add tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-export default function App() {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <h1 className="h-screen flex items-center justify-center">
-      Hello world!
+    <h1 className="h-screen flex items-center justify-center text-7xl text-cyan-500">
+      Hello
     </h1>
-  )
+  );
 }
+
+export default App;
