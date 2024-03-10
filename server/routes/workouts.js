8e91a3101eea6c54f45c23669d8d9cec20b6d9c3
poojith kumar <poojithk956@gmail.com>
2024-03-10 13:37:59 +0530

    commit modules

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -18,8 +19,15 @@ router.patch('/:id',(req,res)=>{
     res.json({msg:"UPDATE a workout"})
 })
 
-router.post('/',(req,res)=>{
-    res.json({msg:"POST a new workout"})
+router.post('/',async (req,res)=>{
+    const {title, load,reps} = req.body;
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+   
 })
 
 module.exports = router
